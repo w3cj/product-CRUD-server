41b205e46b94296fb1f5bb4c44fac78a76f190c0
CJ R <cj@null.computer>
2018-02-19 20:33:16 -0700

    :sparkles: Add cors!

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({
