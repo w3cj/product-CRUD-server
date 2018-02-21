eaf83b3e8c1ac9b64891f93ed61d66dd5a317bc0
CJ R <cj@null.computer>
2018-02-20 21:42:55 -0700

    :rocket: Prepare for deploy

--- knexfile.js
+++ knexfile.js
@@ -3,5 +3,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/cjs_store'
-  }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  },
 };
